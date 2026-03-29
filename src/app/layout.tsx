6a6cf08f0232292fde90624c0e45d1bf691fe2aa
Alexander Burfoot <alex@Alexanders-MacBook-Pro.local>
2026-03-30 01:25:59 +1100

    Add Google site verification tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ export const metadata = {
   title: 'Shoreline Tutoring | Premium One-on-One Tutoring | Sydney',
   description: 'Premium one-on-one tutoring designed to dramatically improve your results through constant support.',
   keywords: 'tutoring, private tutor, HSC tutoring, IB tutoring, maths tutoring, English tutoring, Sydney tutor, one on one tutoring',
+  verification: {
+    google: 'Wv-KvBbu8SvKDVnYb6cXI-IFmJPcZIt2-BUDb_B5BYU',
+  },
   openGraph: {
     type: 'website',
     title: 'Shoreline Tutoring - Exclusive, Individualized, Effective',
